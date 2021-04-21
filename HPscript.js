3a31c7a97c897c939a3346f9e629331706bde85f
Simone Bilotta <67840842+ArmorOfBerserk@users.noreply.github.com>
2021-04-21 17:29:14 +0200

    test with the script for read txt pt.3

--- HPscript.js
+++ HPscript.js
@@ -47,18 +47,57 @@ async function load_page(lang) {
 
     var ita = [];
     var eng = [];
-    var text;
 
     await $.get('res/paragraph/file_ita_eng.txt', function (data) {
-        text  = data;
-    }, 'text');
+        var content_file = data.split("\n");
 
-    return text;
+        var flag_ita = false;
+        var flag_eng = false;
+
+        for (var i = 0; i < content_file.length; i++) {
+            if(content_file[i] != "\r" && content_file[i] != ""){
+
+                if(content_file[i] == "--ITA--\r"){
+                    flag_ita = true;
+                    flag_eng = false;
+                    continue;
+                }
+
+                if(content_file[i] == "--ENG--\r"){
+                    flag_eng = true;
+                    flag_ita = false;
+                    continue;
+                }
+
+                if(flag_ita)
+                    ita.push(content_file[i]);
+
+                if(flag_eng)
+                    eng.push(content_file[i]);
+
+            }
+        }
+
+        /*/!*Posizione 0 per disclaimer*!/*/
+        document.getElementById('disclaimer').innerText = ita[0];
+        /*/!*Posizione 1 per progetto*!/*/
+        document.getElementById('project').innerText = ita[1];
+        /*/!*Posizione 2 per AGGIORNAMENTI*!/*/
+        document.getElementById('date').innerText = ita[2].split(" - ")[0];
+        document.getElementById('novita').innerText = ita[2].split(" - ")[1];
+
+        if( lang == "ITA")
+            return ita;
+
+        else
+            return eng;
+
+    }, 'text');
 }
 
 /*
     await $.getJSON('res/paragraph/file_ita_eng.txt', function (file){
-       var content_file = file.split("\n");
+
         console.log(content_file);
         var flag_ita = false;
        var flag_eng = false;
